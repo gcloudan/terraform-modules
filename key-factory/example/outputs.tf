6927e791405fa148091692ca86da3bc77879b96a
Danny Tran <danny.tran@kasna.com.au>
2021-10-08 12:20:18 +1100

    chore: update readme

--- key-factory/example/outputs.tf
+++ key-factory/example/outputs.tf
@@ -2,6 +2,14 @@ output "keyring_self_link" {
   value = module.kms-keyring.self_link
 }
 
+output "keyring_id" {
+  value = module.kms-keyring.id
+}
+
+output "keyring_name" {
+  value = module.kms-keyring.name
+}
+
 output "keys_self_link" {
   value = module.kms-keyring.keys
 }
